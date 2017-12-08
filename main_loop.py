17a035ed0f9236f582782fc20c8e3fae943f1195
deltaalpha33 <dalexakos@gmail.com>
2017-12-08 18:13:10 -0500

    started working on pygame

--- main_loop.py
+++ main_loop.py
@@ -1,3 +1,5 @@
+import sys, pygame
+
 class Entity():
 	
 	def __init__(self, xCord, yCord, texturePath, collisionRadius):
@@ -10,11 +12,20 @@ class Entity():
 		pass
 
 	def move(self):
-		self.xCord += 1
+		self.xCord -= 1
 
 	def display(self):
 		pass
 
+class Fish(Entity):
+	def __init__(self, xCord, yCord, texturePath, collisionRadius):
+		super().__init__(xCord, yCord, texturePath, collisionRadius)
+
+	def hit(self, gameState):
+		gameState.score += 1
+
+	def display(self):
+		pass
 
 class Checkpoint():
 	def __init__(self, textToDisplay):
@@ -27,9 +38,10 @@ class GameState():
 	def __init__(self):
 		#each stage unlocks an entitiy
 		#fish, pollution, boat
-		self.possibleEntities = list(False, False, False)
+		self.possibleEntities = [False, False, False]
 		self.timeCounter = 0
 		self.hp = 100
+		self.score = 0
 		self.isEnd = False
 		self.entityList = list()
 		self.checkpointList = list()
@@ -37,28 +49,57 @@ class GameState():
 		#shark is just a position on the y axis
 		self.sharkPos = 0
 
-	def updateMotionBar(self):
+
+		#initialize pygame
+
+		pygame.init()
+
+		size = self.width, self.height = 1000, 400
+
+		self.screen = pygame.display.set_mode(size)
+
+	def updateTime(self):
 		"""checks if there is a new checkpoint, and enters it. then it moves the time"""
 		self.timeCounter += 1
 
 	def hitShark(self, e):
 		"""returns whether the entity hit the shark"""
 		pass
-	def updateSharkPos(self):
+	def updateSharkPos(self, num):
 		"""if player moves up, move shark up, if player moves down, move shark down"""
-		pass
+		self.sharkPos += num
+		print("shark pos" + str(self.sharkPos))
 
 	def drawState(self):
 		pass
 
-	def addNewEntities(self):
-		pass
+	def addNewEntities(self, n=1):
+		for x in range(n):
+			e = Fish(10, 10, "" , 1)
+			self.entityList.append(e)
+
+	def moveEntities(self):
+		for e in self.entityList:
+			e.move()
 
 
 gameState = GameState()
-while(not gameStae.isEnd):
-	gameState.updateSharkPos()
-	gameState.updateMotionBar()
+gameState.addNewEntities()
+while(not gameState.isEnd):
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT: sys.exit()
+
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_w:
+				#up
+				gameState.updateSharkPos(-1)
+				
+			if event.key == pygame.K_s:
+				#down
+				gameState.updateSharkPos(1)
+
+	gameState.updateTime()
+
 	for e in gameState.entityList:
 		e.move()
 		if(gameState.hitShark(e)): #checks to see if entity hit the shark
@@ -66,5 +107,7 @@ while(not gameStae.isEnd):
 
 	gameState.addNewEntities()
 
-	gameState.move()
+	gameState.moveEntities()
 	gameState.drawState()
+
+	pygame.time.wait(10)
